96589330d40e9c2ab8971fc05637ed8a1120ac56
ImIsaacTbh <isaac@asterias.co.uk>
2026-02-05 11:56:55 +0000

    basically nothing different just pushing to work on different device

--- src/net/obsc/OBSCTweaks/Events.java
+++ src/net/obsc/OBSCTweaks/Events.java
@@ -3,23 +3,36 @@ package net.obsc.OBSCTweaks;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import net.obsc.OBSCTweaks.OBSCTweaks;
+
+import java.util.Dictionary;
+import java.util.Map;
 
 public class Events implements Listener {
 
     public void Print(String fart)
     {
         Bukkit.getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.LIGHT_PURPLE + "ObscureTweaks" + ChatColor.WHITE + "] " +  ChatColor.WHITE + fart);
+    }
+
+    @EventHandler
+    public void PrepareItemEnchantEvent(PrepareItemEnchantEvent e)
+    {
+        var offers = e.getOffers();
+        for (int i = 0; i < offers.length; i++)
+        {
+            offers[i].setCost(2^i);
+        }
     }
 
     @EventHandler
@@ -36,6 +49,13 @@ public class Events implements Listener {
     public void InventoryClickEvent(InventoryClickEvent event) {
         if( event.getClickedInventory() == null ) {
             return;
+        }
+        if(event.getAction() == InventoryAction.DROP_ALL_CURSOR)
+        {
+            if(event.getCurrentItem().getType() == Material.ENCHANTED_BOOK)
+            {
+
+            }
         }
         if(event.getClickedInventory().getType() == InventoryType.ENCHANTING) {
             if(event.getCurrentItem().getType() == Material.LAPIS_LAZULI) {
